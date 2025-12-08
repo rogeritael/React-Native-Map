861925e82b5b80434da25754de15d3ce1d2b4869
Roger Rosa <rogeritael.contato@gmail.com>
2025-12-08 17:02:34 -0300

    Adicionando as importações do expo location e recuperando as informações de localização do usuário

--- App.tsx
+++ App.tsx
@@ -1,8 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
 import { styles } from './styles';
 import { View } from 'react-native';
+import {
+  requestForegroundPermissionsAsync, //Solicitar permissão para recuperar a localização
+  getCurrentPositionAsync, //Obter a localização do usuário
+  LocationObject
+} from 'expo-location'
 
 export default function App() {
+  const [location, setLocation] = useState<LocationObject | null>(null)
+  const [loading, setLoading] = useState(true)
+  
+  async function requestLocationPermissions(){
+    const { granted } = await requestForegroundPermissionsAsync()
+
+    if(granted){
+      const currentPosition = await getCurrentPositionAsync()
+      setLocation(location)
+      console.log("Localização", currentPosition)
+      console.log("Localização", location)
+    }
+  }
+
+  useEffect(() => {
+    requestLocationPermissions()
+  },[])
+
   return (
     <View style={styles.container}>
 
